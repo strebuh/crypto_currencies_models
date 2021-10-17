ee5b0f54c86b9279dd8436f2a836d51cf8142875
strebuh <hubert.smieciuch@gmail.com>
2021-10-17 02:27:16 +0200

    correction of creating list of currencies

--- scripts/0_crypto_pairs.R
+++ scripts/0_crypto_pairs.R
@@ -1,5 +1,6 @@
 library(rvest)
 library(data.table)
+library(stringi)
 
 # ---------------------------------------------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------------------------------------------
@@ -7,41 +8,42 @@ library(data.table)
 
 
 # scrape names of cryptocurrencies
-coinmarketcap <- xml2::read_html("https://coinmarketcap.com/")
-crypto_names <- coinmarketcap %>%
-  html_nodes(".cmc-link") %>%
-  html_text() 
-
-# get onlu part that contains currencies data
-crypto_names <- crypto_names[18:length(crypto_names)]
-
-# transform vector to dataframe
-top100_crypto <- as.data.frame(matrix(crypto_names, ncol = 4,  byrow = TRUE), stringsAsFactors = FALSE)
-
-# drop lines that do not contain relevant infroamtion
-top100_crypto <- top100_crypto[,-4]
-
-# raname columns
-names(top100_crypto) <- c("Name", "Price", "Capitalization")
-
-# drop last rows, that neither contain relevant information
-top100_crypto <- top100_crypto[-(101:nrow(top100_crypto)),]
-
-# drop dollar signs and commas as thousands delimiters 
-top100_crypto[,2:3] <- lapply(top100_crypto[,2:3], function(x){
-  as.numeric(gsub("\\$|,", "", x))
+general_info = lapply(1:3, function(page){
+  page_content <- xml2::read_html(paste0('https://coinmarketcap.com/?page=', page))
+  table <- page_content %>%
+    html_nodes(xpath = '//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[5]/table[1]') %>%
+    html_table() %>% .[[1]] 
 })
+general_info = rbindlist(general_info)
+general_info[, c("V1", "#", 
+                 "Name", 
+                 "Price",
+                 "24h %",
+                 "7d %", 
+                 "Market Cap", 
+                 "Volume(24h)",
+                 "Circulating Supply", 
+                 "Last 7 Days", "V2"
+                 ) := .(
+                   NULL, NULL,
+                   stri_replace(Name, "", regex = "(\\d+\\w+Buy)$"),
+                   stri_replace_all(`Price`, "", regex = "\\$|,"),
+                   stri_replace(`24h %`, "", fixed = "%"),
+                   stri_replace(`7d %`, "", fixed = "%"),
+                   stri_replace_all(`Market Cap`, "", regex = "\\$\\d+(\\.\\d+\\w\\$)*|,"),
+                   stri_replace_all(stri_extract(`Volume(24h)`, regex = "(\\$)|(\\s\\w{3,})$"), "", regex = "\\$|,"),
+                   stri_replace_all(`Circulating Supply`, "", regex = "(\\s\\w{3,}$)|,"),
+                   NULL, NULL)]
 
-# transform to lower cases and replace . to dash, and space to dash in currencies names
-top100_crypto$Name <- stringr::str_to_lower(gsub("\\.", "-", top100_crypto$Name)) 
-top100_crypto$Name <- gsub("\\s", "-", top100_crypto$Name)
+# general_info = general_info[complete.cases(general_info),]
 
-# saveRDS(top100_crypto, "./data/top100_crypto.RDS")
-top100_crypto <- readRDS("./data/top100_crypto.RDS")
+
+saveRDS(general_info, "./data/top100_crypto.RDS")
+general_info <- readRDS("./data/top100_crypto.RDS")
 
 # create dataframe with combinations of all currencies
-all_combinations <- as.data.frame(t(combn(top100_crypto$Name, 2)), stringsAsFactors = F)
-dim(all_combinations) # [1] 4950    2
+all_combinations <- as.data.frame(t(combn(general_info$Name, 2)), stringsAsFactors = F)
+dim(all_combinations) # [1] 44850     2
 
 # save
-# saveRDS(all_combinations, "./data/all_combinations.RDS")
+saveRDS(all_combinations, "./data/all_combinations.RDS")
